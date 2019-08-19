1f3ba299e61397b3286819e3498b4f2fba424c53
Akash <er.akashmajumder@gmail.com>
2019-08-19 12:03:45 -0700

    Added tup and nested function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    let tup = (500, 6.4, 1);
+    anotherfunction(tup);
+}
+fn anotherfunction(tup: (i32,f64,u8)){
+ println!("Value of y is {}",tup.1);
 }
