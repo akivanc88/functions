f58957140c8c3a9af3c9235b0958bddfbab1b5fa
Akash <er.akashmajumder@gmail.com>
2019-08-19 12:07:18 -0700

    Added return function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world!");
     let tup = (500, 6.4, 1);
-    anotherfunction(tup);
+    println!("Value of z is {}",anotherfunction(tup));
 }
-fn anotherfunction(tup: (i32,f64,u8)){
+fn anotherfunction(tup: (i32,f64,u8))-> u8{
  println!("Value of y is {}",tup.1);
+tup.2
 }
